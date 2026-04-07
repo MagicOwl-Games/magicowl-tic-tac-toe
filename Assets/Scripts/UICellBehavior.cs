fa2f9600a60b0b2e6b7b8371eb7b29141d3be915
Lucas Ribeiro <martins7ribeiro@gmail.com>
2026-04-06 21:58:21 -0300

    changes to Cell.cs and UICellBehaviour.cs

--- Assets/Scripts/UICellBehavior.cs
+++ Assets/Scripts/UICellBehavior.cs
@@ -1,6 +1,71 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UICellBehavior
+namespace magicowl.tictactoe.script
 {
+    public class UICellBehavior: MonoBehaviour
+    {
+        [SerializeField] private Cell cell;
+        [SerializeField] private Button button;
+        [SerializeField] private Image image;
+        [SerializeField] private Color defaultColor;
+        [SerializeField] private Color winColor;
+        [SerializeField] private Color loseColor;
+
+        private Sprite xSprite;
+        private Sprite oSprite;
+        private Sprite emptySprite;
+
+        private void Awake()
+        {
+            xSprite = Resources.Load<Sprite>("x");
+            oSprite = Resources.Load<Sprite>("o");
+            emptySprite = Resources.Load<Sprite>("empty");
+        }
+
+        private void Start()
+        {
+            button.onClick.AddListener(OnButtonClick);
+        }
+
+        private void OnEnable()
+        {
+            cell.OnValueChanged += OnValueChanged;
+            cell.OnGameFinished += OnGameFinished;
+        }
+
+        private void OnDisable()
+        {
+            cell.OnValueChanged -= OnValueChanged;
+            cell.OnGameFinished -= OnGameFinished;
+        }
+
+        private void OnValueChanged(int cell, int newValue)
+        {
+            image.sprite = newValue == 1 ? xSprite : oSprite;
+
+            if (newValue != 0) return; //game restart
+
+            image.sprite = emptySprite;
+            image.color = defaultColor;
+        }
+
+        private void OnGameFinished(bool isGameWin)
+        {
+            image.color = isGameWin ? winColor : loseColor;
+        }
+
+        private void OnButtonClick()
+        {
+            if (!cell.IsInteractive) return;
+
+            if (cell.Value == 0)
+            {
+                bool isPlayerXTurn = TurnManager.Instance.GetTurn();
+                int newValue = isPlayerXTurn ? 1 : 2;
+                cell.SetValue(newValue);
+            }
+        }
+    }
     
 }
